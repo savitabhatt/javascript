e64b66b28dd1feb70036129201379174d8648594
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-05 06:37:45 +0000

    Typesof

---
+++ 02_dataTypes.js
@@ -0,0 +1,27 @@
+// Primativ Datatype 7types: string, number, Boolean,null, undefined,symbol,BigInt
+
+// (Reference) Non-Primtive Datatype: Array, Objets, functions.
+
+
+//  const id = Symbol('123')
+//  const anotherId = Symbol('123')
+//  console.log(id===anotherId);
+
+// const heros= ["shaktiman", "naagraj","doga"]
+
+// {
+//     name:"Shweta",
+//     age:23,
+// } Object
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof null);
+// console.log(typeof heros);
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(typeof anotherId);
