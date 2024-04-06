841da60da0e26b69be1ca1a3536a0087a77064f5
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-06 07:06:13 +0000

    array basic

---
+++ .devcontainer/01_basics/arrays.js
@@ -0,0 +1,13 @@
+// array
+const myArr =[0,1,2,3,4,5]
+const myHeros =["Thepowersgirls","mojojo"]
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+// Array methods
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift();
+console.log(myArr);
