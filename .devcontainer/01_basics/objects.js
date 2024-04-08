59cf81f7ae4a35d92b807661bdf0e09350aa85da
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-08 13:11:20 +0000

    Object literals

---
+++ .devcontainer/01_basics/objects.js
@@ -0,0 +1,32 @@
+// Object literals?\
+
+// const mySyc = Symbol("edkdi")
+// const JsUser={
+//     name:"shweta",
+//     age:18,
+//     [mySyc]:"ek_sb_ki_keydi",
+//     lodcation:"Delhi",
+//     email:"pokhriyial.ldh@3451gmail.co",
+//     isLoggedIn:false,
+//     lastLoginDays:["Monday","Saturday"]
+// }
+// console.log(JsUser.email)
+// console.log (JsUser['email'])
+// console.log(JsUser[mySyc])
+
+// JsUser.email = "bhatt@sarehi10chutiya h"
+// // Object.freeze(JsUser)
+// JsUser.email ="skdfjhd@dog.com"
+// // console.log(JsUser['email'])
+
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
+// console.log(JsUser.greeting());
+// console.log(JsUser.grettingTwo());
+
+
+//------------Singleton----------
