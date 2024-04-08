84ccba8f9836bfdfe2ac78d5f5d1a23f05356590
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-08 10:08:49 +0000

    array basic

--- .devcontainer/01_basics/arrays.js
+++ .devcontainer/01_basics/arrays.js
@@ -1,8 +1,8 @@
 // array
-const myArr =[0,1,2,3,4,5]
-const myHeros =["Thepowersgirls","mojojo"]
-const myArr2 = new Array(1,2,3,4)
-console.log(myArr[1]);
+// const myArr =[0,1,2,3,4,5]
+// const myHeros =["Thepowersgirls","mojojo"]
+// const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[1]);
 
 // Array methods
 // myArr.push(6)
@@ -10,4 +10,48 @@ console.log(myArr[1]);
 // myArr.pop()
 // myArr.unshift(9)
 // myArr.shift();
-console.log(myArr);
+// console.log(myArr);
+
+// console.log("A", myArray);
+// const myn1 = myArr.slice(1,3)
+// console.log(myn1);
+// console.log("B",myArray);
+
+// const myn2= myArray.splice(1,3)
+// console.log("C",myArr);
+
+// console.log(myn2);
+
+
+
+//--------Push----------
+const marvel_heros=["thor", "Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// Concat
+// const all_heros= marvel_heros.concat(dc_heros)
+// console.log(all_heros);
+
+
+// Spread*
+// const all_new_heros =[...marvel_heros,...dc_heros]
+// console.log(all_new_heros);
+
+
+// const another_array=[1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+// // ----------Data formate change into Array------------
+// console.log(Array.isArray("Shweta"))
+// console.log(Array.from("Shweta"))
+// console.log(Array.from({name:"hitesh"}))----interview---
+
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+
+// console.log(Array.of(score1,score2, score3));
