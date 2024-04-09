a731c693ab7f1d4e6210c115f476184967c55d11
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-09 07:15:20 +0000

    obj2

--- .devcontainer/01_basics/objects.js
+++ .devcontainer/01_basics/objects.js
@@ -30,3 +30,38 @@
 
 
 //------------Singleton----------
+// // const tinderUser = new Object ()
+// const tinderUser ={}
+// tinderUser.id = "123abc"
+// tinderUser.name ="Shivi"
+// tinderUser.isLoggedIn = false
+
+// // // console.log(tinderUser);
+
+// const regularUser={
+//     emial:"some@gamol.cojm",
+//     fullname:{
+//         userfullname:{
+//             firstname:"sherta",
+//             lastname: "Bhatt"
+
+//         }
+//     }
+// }
+// // console.log(regularUser.fullname?.userfullname.lastname);
+
+
+// const obj1 = {1:"a",2:"b"}
+// const obj2 ={3:"a", 4:"b"}
+// const obj4 ={5:"a", 6:"b"}
+
+// // const obj3={ obj1, obj2}
+// // const obj3= object.assign({}, obj1, obj2, obj4)
+// const obj3={...obj1, ...obj2}
+// // console.log(obj3);
+
+// // users[1].email
+// console.log(tinderUser);
+// console.log(object.keys(tinderUser));
+// console.log(objec.values(tinderUser));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
