f74f5a9d024275f5d1c9a74f6e00d55378c63de0
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-10 05:53:09 +0000

    function

---
+++ .devcontainer/01_basics/function.js
@@ -0,0 +1,19 @@
+
+function sayMyName(){
+    console.log("S");
+    console.log("H");
+    console.log("W");
+    console.log("E");
+    console.log("t");
+    console.log("A");
+     
+}
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(3,5)
+console.log("Result:", result);
