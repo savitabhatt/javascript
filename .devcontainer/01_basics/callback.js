2eb7dd262af7b98fc422fb6f4a522c5e90589530
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-12 10:36:57 +0000

    cllbck

--- .devcontainer/01_basics/callback.js
+++ .devcontainer/01_basics/callback.js
@@ -1,22 +1,36 @@
-const calculate = (a,b,operation)=>{
-    return operation(a,b);
-};
+// const calculate = (a,b,operation)=>{
+//     return operation(a,b);
+// };
 
-// method 1 Addition
-const addition=calculate(3,4,function(num1,num2){
-    return num1 + num2;
-});
-console.log(addition);
+// // method 1 Addition
+// const addition=calculate(3,4,function(num1,num2){
+//     return num1 + num2;
+// });
+// console.log(addition);
  
 
-// method 2 Subraction
-const subtraction = (a,b)=>a-b;
-const subResult = calculate(8,3,subtraction)
-console.log(subResult);
+// // method 2 Subraction
+// const subtraction = (a,b)=>a-b;
+// const subResult = calculate(8,3,subtraction)
+// console.log(subResult);
 
-// method 3 Multiplication
-function multiply(a,b){
-    return a*b;
-}
-const myResult = calculate(3,2,multiply);
-console.log(myResult);
+// // method 3 Multiplication
+// function multiply(a,b){
+//     return a*b;
+// }
+// const myResult = calculate(3,2,multiply);
+// console.log(myResult);
+
+// const a = [4,1,6,-2,-5,3,2,-8,6,7];
+// const firstNeg =(num)=>{
+//     return num < 0;
+// };
+// // const result = a.find(firstNeg);
+// // console.log(result);
+
+// const result = a.findIndex(firstNeg);
+// console.log(result);
+
+// a.forEach((num,i)=>{
+//     console.log("Array num",num,i);
+// });
