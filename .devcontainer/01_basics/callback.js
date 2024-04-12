62acc24bd288962898d192479b657543d8c7f530
Savita bhatt <124357228+savitabhatt@users.noreply.github.com>
2024-04-12 08:25:30 +0000

    callback function

---
+++ .devcontainer/01_basics/callback.js
@@ -0,0 +1,22 @@
+const calculate = (a,b,operation)=>{
+    return operation(a,b);
+};
+
+// method 1 Addition
+const addition=calculate(3,4,function(num1,num2){
+    return num1 + num2;
+});
+console.log(addition);
+ 
+
+// method 2 Subraction
+const subtraction = (a,b)=>a-b;
+const subResult = calculate(8,3,subtraction)
+console.log(subResult);
+
+// method 3 Multiplication
+function multiply(a,b){
+    return a*b;
+}
+const myResult = calculate(3,2,multiply);
+console.log(myResult);
